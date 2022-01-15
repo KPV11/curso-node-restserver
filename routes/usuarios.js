65c88c3a375ea6c5841da8ca5424f863496080ce
Kevin Palomino <kpalominov11@gmail.com>
2022-01-15 18:56:01 -0500

    Usuarios CRUD listo -15-01-2022

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,16 +1,40 @@
 
 const { Router } = require('express');
+const {check} = require('express-validator');
+
+const { ValidarCampos } = require('../middlewares/validar-campos');
+const { esRoleValido, emailExist, existeUsuarioPorId } = require('../helpers/db-validators');
+
 const { usuariosGet, usuariosPut, usuariosPost, usuariosDelete, usuariosPatch } = require('../controllers/usuarios');
+
+
 
 const router = Router();
 
 router.get('/',usuariosGet);
 
-router.put('/:id',usuariosPut);
+router.put('/:id',[
+    check('id','No es ID válido').isMongoId(),
+    check('id').custom(existeUsuarioPorId),
+    check('rol').custom(esRoleValido),
+    ValidarCampos
+],usuariosPut);
 
-router.post('/',usuariosPost);
+router.post('/',[
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password debe ser mas de 6 letras').isLength({min:6}),
+    check('correo', 'El correo no es válido').isEmail(),
+    check('correo').custom(emailExist),
+    //check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE','USER_ROLE']),
+    check('rol').custom(esRoleValido),
+    ValidarCampos
+],usuariosPost);
 
-router.delete('/',usuariosDelete);
+router.delete('/:id',[
+    check('id','No es ID válido').isMongoId(),
+    check('id').custom(existeUsuarioPorId),
+    ValidarCampos
+],usuariosDelete);
 
 router.patch('/',usuariosPatch);
 
